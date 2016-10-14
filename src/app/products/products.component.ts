ce717d1710a36dd1ca508b995a37418b659a855e
Carlos Matheu Armengol <info@plastikaweb.com>
2016-10-14 21:58:53 +0200

    product service

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from './product';
+import {ProductService} from './product.service';
 
 @Component( {
     selector: 'app-products',
@@ -7,63 +8,15 @@ import {IProduct} from './product';
     styleUrls: ['./products.component.css']
 } )
 export class ProductsComponent implements OnInit {
+
     listProductTitle: string = 'Product List';
     imageWidth: number = 20;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = '';
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 5,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 1,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        },
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2016",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-        },
-        {
-            "productId": 10,
-            "productName": "Video Game Controller",
-            "productCode": "GMG-0042",
-            "releaseDate": "October 15, 2015",
-            "description": "Standard two-button video game controller",
-            "price": 35.95,
-            "starRating": 3,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-        }
-    ];
+    listFilter: string;
+    products: IProduct[];
+
+    constructor(private productService: ProductService){}
 
     toggleImage() {
         this.showImage = !this.showImage;
@@ -74,6 +27,6 @@ export class ProductsComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.info('init products component');
+        this.products = this.productService.getProducts();
     }
 }
